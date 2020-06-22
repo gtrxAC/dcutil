3835e1ee2a4a5f797e7a08f217cf82f6a4a7c288
gtrxAC <59177874+gtrxAC@users.noreply.github.com>
2020-06-22 14:01:25 +0300

    fix bug with avatar command

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,10 +6,10 @@ module.exports = {
 	async run(message, args, db) {
 		// Get the user from the arguments, or default to the author.
 		const name = args.join(' ');
-		const member = message.mentions.members.first()
+		const member = (args.length ? message.mentions.members.first()
 		|| message.guild.members.cache.find(m => m.displayName.startsWith(name))
 		|| message.guild.members.cache.get(args[0])
-		|| message.member;
+		|| message.member : message.member);
 
 		// Get their avatar URL.
 		const url = member.user.avatarURL({format: 'png'});
