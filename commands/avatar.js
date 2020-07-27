2e2601d8809f2fb708ba536305df385ade4fbd81
gtrxAC <59177874+gtrxAC@users.noreply.github.com>
2020-07-27 22:59:21 +0300

    fix bug where `avatar` doesn't support usernames

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,17 +2,18 @@ module.exports = {
 	name: 'avatar',
 	aliases: ['pfp'],
 	description: "Get an image from someone's avatar.",
-	usage: '[@user | userid | username]',
+	usage: '[@user | nickname | username | userid]',
 	async run(message, args, db) {
 		// Get the user from the arguments, or default to the author.
 		const name = args.join(' ');
 		const member = (args.length ? message.mentions.members.first()
-		|| message.guild.members.cache.find(m => m.displayName.startsWith(name))
+		|| message.guild.members.cache.find(m => m.displayName.startsWith(name)
+		|| m.user.tag.startsWith(name))
 		|| message.guild.members.cache.get(args[0])
 		|| message.member : message.member);
 
 		// Get their avatar URL.
-		const url = member.user.avatarURL({format: 'png'});
+		const url = member.user.avatarURL({format: 'png', dynamic: true});
 
 		// Send an attachment containing it.
 		return {files: [url]};
